3b4ab221bdcb01b82f0d1019f051e8c64c9085b4
edmundo papi <edmundopierre@yahoo.com>
2020-08-05 14:21:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,10 +35,10 @@ class Student
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
-  end 
+  end
 
   def save
-     
+
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
