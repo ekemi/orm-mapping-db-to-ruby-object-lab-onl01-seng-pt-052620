8423ced899f085cd2a53b2a5ce099c69412e8fe9
edmundo papi <edmundopierre@yahoo.com>
2020-08-05 15:06:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -107,7 +107,7 @@ class Student
            ORDER BY students.id
            LIMIT 1
            SQL
-    DB[:conn].execute.map do|row|
+    DB[:conn].execute(sql).map do|row|
         self.new_from_db(row)
       end.first
          end
