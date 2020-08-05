b0c23ffb663acace94d530db84f6b3ed7e9202d7
edmundo papi <edmundopierre@yahoo.com>
2020-08-05 14:54:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,7 @@ class Student
 
   end
 
-  def self.first_X_students_in_grade_10(number)
+  def self.first_X_students_in_grade_10 (number)
     sql = <<-SQL
             SELECT *
             FROM students
